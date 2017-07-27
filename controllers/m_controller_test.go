d7db4b126be826418ca9bca33b8cf81616ff5b87
Satyam Zode <satyamzode@gmail.com>
2017-07-27 12:09:17 +0530

    Improve test for GetVolumeStatus handler

--- controllers/m_controller_test.go
+++ controllers/m_controller_test.go
@@ -11,10 +11,22 @@ func TestGetVolumeStatus(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	requestRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(GetVolumeStatus)
 	handler.ServeHTTP(requestRecorder, req)
+
 	if requestRecorder.Code != http.StatusOK {
 		t.Errorf("Handler returned wrong status code. Got %d , Want %d", requestRecorder.Code, http.StatusOK)
+	} else {
+		t.Logf("Handler returned correct status code %d", requestRecorder.Code)
+	}
+
+	expected := `{"alive" : true}`
+
+	if expected != requestRecorder.Body.String() {
+		t.Errorf("Handler returned wrong body, Got %s Want %s", requestRecorder.Body.String(), expected)
+	} else {
+		t.Logf("Handler returned correct body %s", requestRecorder.Body.String())
 	}
 }
